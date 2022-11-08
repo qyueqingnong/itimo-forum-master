cdb3dd17db136c8d27b9dd53f0b488ddb626ed36
qyueqingnong <402621356@qq.com>
2022-11-09 01:17:18 +0800

    initspringboot

---
+++ src/main/java/cc/itimo/itimoforummaster/ItimoForumMasterApplication.java
@@ -0,0 +1,13 @@
+package cc.itimo.itimoforummaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItimoForumMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItimoForumMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/itimo/itimoforummaster/ItimoForumMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.itimo.itimoforummaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItimoForumMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
